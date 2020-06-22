bb58317175c63218664c937d9b3dd1c807b383a7
captainvlad <zadorozhnyy@ucu.edu.ua>
2020-06-22 13:58:23 +0300

    Initial matrix

--- main.cpp
+++ main.cpp
@@ -15,9 +15,12 @@ int main(int argc, char *argv[])
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
+//    char processor_name[MPI_MAX_PROCESSOR_NAME];
+//    int name_len;
+//    MPI_Get_processor_name(processor_name, &name_len);
+
+    std::vector<double> partial_matrix;
+    int partial_matrix_size;
 
     if (world_rank == 0) {
         auto config_data = read_config("../config.json");
@@ -34,44 +37,37 @@ int main(int argc, char *argv[])
         auto matrix = *initialize_matrix(h, w);
         add_all_temperatures_to_matrix(&matrix, &config_data, h, w);
 
-        int back_buffer;
-        std::vector<double> c;
-
         for (int i = 1; i < world_size; i++){
-            auto matrix_1 = *cut_matrix_stripe(&matrix, h, w, world_size - 1, i - 1);
-//            represent_matrix(matrix_1, 50, 100);
-//            std::cout << "\n\n";
-            MPI_Send(new int( matrix_1.size() ) , 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send(&matrix_1[0] , matrix_1.size(), MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+            partial_matrix = *cut_matrix_stripe(&matrix, h, w, world_size - 1, i - 1);
+            MPI_Send(new int( partial_matrix.size() ) , 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&partial_matrix[0] , partial_matrix.size(), MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
         }
 
         for (int i = 1; i < world_size; i++){
-            MPI_Recv(&back_buffer, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            c.resize(back_buffer);
-            MPI_Recv(&c[0], back_buffer, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            put_into(&matrix, &c, 100, 100, world_size - 1, i - 1);
+            MPI_Recv(&partial_matrix_size, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            partial_matrix.resize(partial_matrix_size);
+
+            MPI_Recv(&partial_matrix[0], partial_matrix_size, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            put_into(&matrix, &partial_matrix, 100, 100, world_size - 1, i - 1);
         }
 
         represent_matrix(&matrix, 100, 100);
 
-    }
-    else {
-        int size;
-        std::vector<double> b;
+    } else {
 
-        MPI_Recv(&size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        b.resize(size);
+        MPI_Recv(&partial_matrix_size, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        partial_matrix.resize(partial_matrix_size);
 
-        MPI_Recv(&b[0], size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&partial_matrix[0], partial_matrix_size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        for (int i = 0; i < b.size(); i++){
-            if (b[i] == 0){
-                b[i] = world_rank;
+        for (int i = 0; i < partial_matrix_size; i++){
+            if (partial_matrix[i] == 0){
+                partial_matrix[i] = world_rank;
             }
         }
 
-        MPI_Send(&size ,1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-        MPI_Send(&b[0] , b.size(), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&partial_matrix_size ,1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&partial_matrix[0] , partial_matrix_size, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
